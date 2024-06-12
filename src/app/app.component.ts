9b179e0df6a9d6ca5af7bfd7a5ded1124ac23cff
JohannaPuerchambud <jepuerchambudp@utn.edu.ec>
2024-06-12 04:18:39 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'APP-MODULO-SEGURIDAD';
+  message: string = 'Hola Joha';
+  
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+      this.apiService.getMessage().subscribe(
+          data => this.message = data.message,
+          error => console.error(error)
+      );
+  }
 }
